2fb2fbd4d1389094341adda557d91b4c9f7363bb
jessica you <jessicaxytan@gmail.com>
2021-07-14 10:01:18 -0400

    commit tree ebef1f1451fdcab37eb18970239920fd2c755c61

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    message = []
+    attendees.each do |name|
+        message << "Hello, my name is #{name}."
+    end
+    message
+end
+
+def assign_rooms(names)
+    message = []
+    names.each_with_index do |name, index|
+        message << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+    message
+end
+
+def printer(names)
+    batch_badge_creator(names).each do |name|
+        puts name
+    end
+
+    assign_rooms(attendees).each do |attendee|
+        puts attendee
+    end
+end
